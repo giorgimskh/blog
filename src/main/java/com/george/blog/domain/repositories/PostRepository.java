332f8c76a625442836325f2a95e86cdff9a10258
giorgimskh <giorgimsxrikadze@gmail.com>
2025-09-19 23:59:27 +0400

    created repositories interface

---
+++ src/main/java/com/george/blog/domain/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.george.blog.domain.repositories;
+
+import com.george.blog.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PostRepository extends JpaRepository<Post, UUID> {
+}
